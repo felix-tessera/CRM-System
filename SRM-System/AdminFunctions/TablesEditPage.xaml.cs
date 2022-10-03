25a94103735079392daeccf51b3ffe0fc6dec543
must-have-for-you <nikitarassvetov@mail.ru>
2022-10-03 13:52:19 +0300

    Add table model

---
+++ SRM-System/AdminFunctions/TablesEditPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace SRM_System.AdminFunctions;
+
+public partial class TablesEditPage : ContentPage
+{
+	public TablesEditPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SRM-System/Models/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRM_System.Models
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public string Seats { get; set; }
+        public string Сondition { get; set; }
+    }
+}
